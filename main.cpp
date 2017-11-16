00623c72933f831db387b26525f90509e2d5ac0e
bushmaks <bushmaks@gmail.com>
2017-11-16 23:40:58 +0300

    Закомментировал все

--- main.cpp
+++ main.cpp
@@ -16,58 +16,58 @@ using namespace std;
 
 const char *FName = "Mass.txt"; // Название исходного файла
 
-long long int GetSizeOfFile(const char* path){
-    ifstream in(path, ifstream::ate | ifstream::binary);
-    return in.tellg();
+long long int GetSizeOfFile(const char* path){ // Функция для вычисления размера файла.
+    ifstream in(path, ifstream::ate | ifstream::binary); // Открываем заданный файл и переставляем курсор в конец.
+    return in.tellg(); // Выдает позицию курсора в файле в битах.
 }
 
 
-int GetNumber(const char* symbols, int length){
-    int res = 0;
-    for (int i = 1; i <= length; i++){
-         res += (symbols[i-1]-'0') * pow(10, length - i);
+int GetNumber(const char* symbols, int length){ // Атрибуты: массив, длина массива и разрядность числа. Функция для определения номера: 1 разрядный, 2 разрядный и т.д.
+    int res = 0; // Результат выполнения
+    for (int i = 1; i <= length; i++){ // Цикл для вычисления разрядности. Например: Число - 150. Вычисление: 100 + 50 + 0.
+        res += (symbols[i-1]-'0') * pow(10, length - i); // i = 1 потому что разрядность начинается с 1. "-'0'" - нужен для конвертации из char в int. (int) не работает.
     }
     return res;
 }
 
 int main(){
-    long long int fileSize = GetSizeOfFile(FName);
-    char fileContent[fileSize];
+    long long int fileSize = GetSizeOfFile(FName); // Размер файла в байт
+    char fileContent[fileSize]; // Массив для файла
    try {
        
-       ifstream fin(FName, ifstream::binary);
+       ifstream fin(FName, ifstream::binary); // Открываем файл в fin, в бинарном виде
        
-        if(!fin.is_open()){
+        if (!fin.is_open()) { // Проверка на чтение файла
             throw "Ошибка чтения файла.";
         }
-        else {
-            for  (int i = 0; !fin.eof(); i++) {
+        else { // Если файл открыт
+            for  (int i = 0; !fin.eof(); i++) { // Цикл для занесения файла в массив. До конца файла
                 fileContent[i] = fin.get();
             }
-            int count = 0;
-            int foundedArraySize;
-            do {
+            int count = 0; // Сетчик для перемещения в массиве fileContent
+            int foundedArraySize; // Найденый размер массива
+            do { // Ищем размер массива. Если не число, то ищем дальше поэлементно
                 foundedArraySize = (int)fileContent[count];
                 count++;
             }
             while (!((int)foundedArraySize > 47 && (int)foundedArraySize < 58));
             
             
-            int Array[foundedArraySize];
-            for (int i = 0, j = count; j < fileSize;){
-                if (fileContent[j] != ' ' && ((int)fileContent[j] > 47 && (int)fileContent[j] < 58)) {
-                    char numberSymbolsArray[10];
-                    int countSymbol = 0;
-                    while ((int)fileContent[j] > 47 && (int)fileContent[j] < 58) {
-                        numberSymbolsArray[countSymbol] = fileContent[j];
+            int Array[foundedArraySize]; // Рабочий массив
+            for (int i = 0, j = count; j < fileSize;i++,j++){ // Цикл поиска чисел массива. j - номер элемента в массиве  count - место после размерности массива.
+                if (fileContent[j] != ' ' && ((int)fileContent[j] > 47 && (int)fileContent[j] < 58)) { // Если не пробел и не число то
+                    char numberSymbolsArray[10]; // Массив для вычисления числа более одного разряда
+                    int countSymbol = 0; // Счетчик разрядности и индекса массива
+                    while ((int)fileContent[j] > 47 && (int)fileContent[j] < 58) { // Пока числа выполняется цикл
+                        numberSymbolsArray[countSymbol] = fileContent[j]; // Присваиваем элемент из файла в массив разрядности
                         countSymbol++;
-                        j++;
+                        j++; // Следущий элемент в файле
                     }
-                    Array[i] = GetNumber(numberSymbolsArray, countSymbol);
+                    Array[i] = GetNumber(numberSymbolsArray, countSymbol); // Присваивание рабочему массиву число из массива разрядности
+                    cout << Array[i] << " "; // Вывод массива из файла
                 }
-                i++;
-                j++;
             }
+            cout << endl;
         }
     }
     catch(exception e) {
